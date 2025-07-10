78448355848e753c93b354b5bd904cfa26c60532
rhenando <rhenandos@hotmail.com>
2025-07-10 20:09:32 +0300

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // 👈 This enables static HTML export (creates `out` folder)
+};
 
 export default nextConfig;
