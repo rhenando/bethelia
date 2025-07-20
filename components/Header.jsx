ba1671131d35e8852484ee63dfa0c653401dcb30
rhenando <rhenandos@hotmail.com>
2025-07-20 19:49:17 +0300

    updated data

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { Heart, ShoppingCart, Search } from "lucide-react";
+import Link from "next/link";
 
 export default function Header() {
   const wishCount = 0;
@@ -8,9 +9,11 @@ export default function Header() {
 
   return (
     <header className='flex items-center justify-between px-4 py-3 border-b border-gray-200 bg-white'>
-      <span className='font-extrabold text-lg tracking-tight text-[var(--primary,theme(colors.blue.700))] select-none'>
-        Bethelia PH
-      </span>
+      <Link href='/'>
+        <span className='font-extrabold text-lg tracking-tight text-[var(--primary,theme(colors.blue.700))] select-none'>
+          Bethelia PH
+        </span>
+      </Link>
 
       <div className='flex items-center gap-4'>
         {/* Search */}
